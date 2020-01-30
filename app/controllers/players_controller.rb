19e70560763bf5f6a56f62eeb8659dc6e96ec66d
Kane Koo <kanekoo@gmail.com>
2020-01-30 07:53:19 -0500

    013020

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,26 @@
 class PlayersController < ApplicationController
 
   def show
+    player = Player.find_by(id: params[:id])
+    render json: player.to_json
   end
 
   def index
+    players=Player.all
+    render json: Player.all, only: [:id, :firstname, :lastname]
+  end
+
+  def create
+
+    player = Player.new(params[:id])
+
+    player.save
+    render json: player.to_json
+  end
+
+  private
+
+  def account_params
+    params.require(:player).permit(:fname, :lname, :position)
   end
 end
